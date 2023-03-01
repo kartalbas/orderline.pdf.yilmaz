f0621e7aea222c7e8ff1af2ea4670489a8027333
Mehmet Kartalbas <kartalbas@gmail.com>
2023-03-01 21:58:25 +0100

    Rechnungen bis '%d.%m.%Y %H:%M:%S'

---
+++ push-rechnungen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# lösche alle unwichtige Dateien
+find ~/orderline.pdf.yilmaz/ -type f -regextype posix-extended -regex '^.*[0-9a-fA-F]{32}\.(xls|pdf)' -exec rm {} \;
+
+# markiere alles für commit
+git add --all
+
+# commit alle Rechnungen
+git commit -m "Rechnungen bis '%d.%m.%Y %H:%M:%S'"
+
+# push alle Rechnungen zu Github
+git push
+
