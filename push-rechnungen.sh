679fe1c6ea08714e226b739b8934fb0789668266
Mehmet Kartalbas <kartalbas@gmail.com>
2023-03-01 22:05:09 +0100

    Rechnungen bis 01.03.2023 22:05:09

--- push-rechnungen.sh
+++ push-rechnungen.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+REPO_PATH="/home/yfadmin/orderline.pdf.yilmaz"
+
 # lösche alle unwichtige Dateien
-find ~/orderline.pdf.yilmaz/ -type f -regextype posix-extended -regex '^.*[0-9a-fA-F]{32}\.(xls|pdf)' -exec rm {} \;
+find "$REPO_PATH" -type f -regextype posix-extended -regex '^.*[0-9a-fA-F]{32}\.(xls|pdf)' -exec rm {} \;
 
 # markiere alles für commit
-git add --all
+git -C "$REPO_PATH" add --all
 
 # commit alle Rechnungen
-git commit -m "Rechnungen bis `date +'%d.%m.%Y %H:%M:%S'`"
+git -C "$REPO_PATH" commit -m "Rechnungen bis `date +'%d.%m.%Y %H:%M:%S'`"
 
 # push alle Rechnungen zu Github
 git push
